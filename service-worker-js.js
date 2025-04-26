eea4b76fa5003dfaf3ae89b5e7d9f08dfe057e70
gauthierae <alain.gauthier@gmail.com>
2025-04-26 09:35:30 -0400

    Add files via upload

---
+++ service-worker-js.js
@@ -0,0 +1,69 @@
+// Service Worker pour l'aide-mémoire PWA
+
+const CACHE_NAME = 'aide-memoire-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du service worker et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes réseau
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si la ressource est en cache, la renvoyer
+        if (response) {
+          return response;
+        }
+
+        // Sinon, faire la requête réseau et mettre en cache
+        return fetch(event.request).then(
+          response => {
+            // Vérifier si nous avons reçu une réponse valide
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Cloner la réponse car elle est utilisée par le navigateur et le cache
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
